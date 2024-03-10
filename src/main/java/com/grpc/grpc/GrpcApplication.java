9c2efdc31dd1e199565f6cb044e0f2718c664156
taukilali <taukilali@gmail.com>
2024-03-10 23:59:20 +0530

    first commit

--- src/main/java/com/grpc/grpc/GrpcApplication.java
+++ src/main/java/com/grpc/grpc/GrpcApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GrpcApplication {
 
-	//grpc
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcApplication.class, args);
 	}
